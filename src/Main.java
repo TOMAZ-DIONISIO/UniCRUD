56f952f0d8dfbe1b311f2059378d8f1c50afe32f
TOMAZ-DIONISIO <tomaz.mvdionisio@gmail.com>
2023-11-22 20:33:07 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("*--------------------------------------------------------------*");
+        System.out.println("* Bem Vindo ao UniCRUD!                                        *");
+        System.out.println("* Selecione a opção que pretende:                              *");
+        System.out.println("*--------------------------------------------------------------*");
+
+
+        Hashmap<String, String, String> people = new HashMap<>();
+
+        people.put(101, "Miguel");
+        people.put(102, "Ana");
+        people.put(103, "Pedro");
+        people.put(104, "Rui");
+        people.put(105, "Maria");
+
+        String pessoas = people.get;
+        System.out.println("nº aluno e nome: " + pessoas);
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+        Pessoa nome = new Pessoa ();
+        Pessoa dataNascimento = new Pessoa();
+        Pessoa id = new Pessoa();
+
+    class Aluno extends Pessoa {
+
+        Aluno nAluno = new Aluno();
+        Aluno curso = new Aluno();
+        Aluno cadeiras = new Aluno();
+        Aluno mediaTotal = new Aluno();
+    }
+    class Docente extends Pessoa{
+        Docente cursoDocente = new Docente();
+        Docente cadeiraDocente = new Docente();
+
+    }
+}
